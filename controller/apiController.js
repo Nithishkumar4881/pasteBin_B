74f266dc2f8de1cf1725b03964a17d4d7db4dc71
Nithishkumar4881 <nithisk72@gmail.com>
2026-02-04 22:33:17 +0530

    expired bugs nad views bugs fixed

--- controller/apiController.js
+++ controller/apiController.js
@@ -1,5 +1,5 @@
 const PBmodel = require("../models/model.js");
-const getCurrentTime = require("../currentTime.js")
+const getCurrentTime = require("../currentTime.js");
 const create = async (req, res) => {
   const { content, expiredAt, max_views } = req.body;
 
@@ -9,18 +9,16 @@ const create = async (req, res) => {
 
   const paste = new PBmodel({
     content,
-    expiredAt: expiredAt? getCurrentTime(req)+ expiredAt*1000:null,
+    expiredAt: expiredAt ? Date.now() + expiredAt * 1000 : null,
     max_views,
     id: `paste_${Date.now()}`,
   });
   await paste.save();
 
-  res
-    .status(201)
-    .json({
-      id: paste.id,
-      url: `http://localhost:8001/api/pastes/${paste.id}`,
-    });
+  res.status(201).json({
+    id: paste.id,
+    url: `http://localhost:8001/p/${paste.id}`,
+  });
 };
 const healthz = async (req, res) => {
   res.status(200).json({ ok: true });
@@ -30,23 +28,34 @@ const view = async (req, res) => {
   const pasteId = req.params.id;
   console.log(pasteId);
 
-  const paste = await PBmodel.findOne(
-    { id: pasteId }
-  );
+  const paste = await PBmodel.findOne({ id: pasteId });
 
-  if(!paste){
-   return res.status(404).json({msg:"content id not match"})
+  if (!paste) {
+    return res.status(404).json({ msg: "content id not match" });
   }
-  if (paste.max_views < paste.currentViews){
-    return res.status(403).json({msg:"you have reached out maximum views"})
+  if (paste.max_views && paste.max_views < paste.currentViews) {
+    return res.status(403).json({ msg: "you have reached out maximum views" });
   }
-  if (paste.expiredAt <= Date.now()){
-    return res.status(403).json({msg:"content expired"})
+  if (paste.expiredAt !== null) {
+    console.log(paste.expiredAt);
+
+    if (paste.expiredAt < Date.now()) {
+      return res.status(403).json({ msg: "content expired" });
+    }
   }
 
-  res.status(200).json({content: paste.content, expiredAt: new Date(paste.expiredAt), viewsLeft:paste.max_views - paste.currentViews});
-  paste.currentViews = paste.currentViews+1;
-  await paste.save().catch(e=>console.log(e.message))
+  res
+    .status(200)
+    .json({
+      content: paste.content,
+      expiredAt: paste.expiredAt ? new Date(paste.expiredAt) : "never expired",
+      viewsLeft:
+        paste.max_views !== null
+          ? paste.max_views - paste.currentViews
+          : "unlimitted",
+    });
+  paste.currentViews = paste.currentViews + 1;
+  await paste.save().catch((e) => console.log(e.message));
 };
 
 const viewHTML = async (req, res) => {
@@ -55,30 +64,33 @@ const viewHTML = async (req, res) => {
 
   const paste = await PBmodel.findOne({ id: pasteId });
 
-  if(!paste){
-    return res.status(404).header({"content-type":"HTML"}).send(`<html>
+  if (!paste) {
+    return res.status(404).header({ "content-type": "HTML" }).send(`<html>
     <title>Paste</title>
     <body>
       <h1 style="color:red">content id not match</h1>
     </body>
-  </html>`)
+  </html>`);
   }
-  
-  if (paste.viewsLeft == 0){
+
+  if (paste.viewsLeft == 0) {
     return res.status(404).send(`<html>
     <title>Paste</title>
     <body>
       <h1 style="color:red">you have reached out maximum views</h1>
     </body>
-  </html>`)
+  </html>`);
   }
-  if (paste.expiredAt <= Date.now()){
-    return res.status(404).header({"content-type":"HTML"}).send(`<html>
+  if (paste.expiredAt !== null) {
+    console.log(paste.expiredAt);
+    if (paste.expiredAt < Date.now()) {
+      return res.status(404).header({ "content-type": "HTML" }).send(`<html>
     <title>Paste</title>
     <body>
-      <h1 style="color:red">content expired</h1>
+      <h1 style="color:red">content expired ${new Date(paste.expiredAt)}</h1>
     </body>
-  </html>`)
+  </html>`);
+    }
   }
 
   res.status(200).send(`<html>
@@ -87,7 +99,7 @@ const viewHTML = async (req, res) => {
       <h1 style="color:green">${paste.content}</h1>
     </body>
   </html>`);
-  paste.currentViews = paste.currentViews+1;
+  paste.currentViews = paste.currentViews + 1;
   await paste.save();
 };
 
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 const dbconnection = require("./DBconfig.js");
 const routes = require("./routes/routes.js")
 const route = require("./routes/route.js")
+const cors = require("cors")
 
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 dbconnection();
 app.use("/api", routes)
