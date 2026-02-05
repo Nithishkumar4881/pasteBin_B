56d3fd101f1d7b1820f3d1c965035be3c62e3ba3
Nithishkumar4881 <nithisk72@gmail.com>
2026-02-05 09:39:51 +0530

    response html

--- controller/apiController.js
+++ controller/apiController.js
@@ -33,11 +33,11 @@ const view = async (req, res) => {
   if (!paste) {
     return res.status(404).json({ msg: "content id not match" });
   }
-  if (paste.max_views && (paste.max_views < paste.currentViews)) {
+  if (paste.max_views && (paste.max_views > paste.currentViews)) {
     return res.status(403).json({ msg: "you have reached out maximum views" });
   }
   if (paste.expiredAt !== null) {
-    console.log(paste.expiredAt);
+    // console.log(paste.expiredAt);
 
     if (paste.expiredAt < Date.now()) {
       return res.status(403).json({ msg: "content expired" });
